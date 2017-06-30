09b0a1c65aadbeeba8685d71f7184d7b033174ce
ctrombet <cristian.trombetta@gmail.com>
2017-06-30 12:05:26 +0100

    Puppet manifests

---
+++ vagrant-puppet/puppet/environments/dev/modules/profile/manifests/test.pp
@@ -0,0 +1,4 @@
+class profile::test {
+    include test
+    
+}
---
+++ vagrant-puppet/puppet/environments/dev/modules/test/manifests/init.pp
@@ -0,0 +1,3 @@
+class test {
+notify {"Start to modify the files in /vagrant/environments/dev/modules/test/manifests/init.pp to start developing":}
+}
